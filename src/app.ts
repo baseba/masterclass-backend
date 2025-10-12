5cdd7396c02c89378a74c0a87ae033e3498dc4d8
juaconsio <jconchan@outlook.com>
2025-10-11 21:24:29 -0300

    refactor: update CORS configuration and enhance morgan logging for better request visibility

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,50 @@
-import morgan from "morgan";
-import express from "express";
-import passport from "passport";
-import bodyParser from "body-parser";
-import cors from "cors";
-import authRouter from "./routes/auth";
-import helloRouter from "./routes/hello";
-import adminRouter from "./routes/admin";
-import professorRouter from "./routes/professor/professors";
-import courseRouter from "./routes/course/courses";
-import authenticateJwt from "./middleware/authenticateJwt";
-import slotRouter from "./controllers/slots.controller";
-import sessionRouter from "./routes/course/sessions";
+import morgan from 'morgan';
+import express from 'express';
+import passport from 'passport';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import authRouter from './routes/auth';
+import helloRouter from './routes/hello';
+import adminRouter from './routes/admin';
+import professorRouter from './routes/professor/professors';
+import courseRouter from './routes/course/courses';
+import authenticateJwt from './middleware/authenticateJwt';
+import slotRouter from './controllers/slots.controller';
+import sessionRouter from './routes/course/sessions';
 
-const allowedOrigins = ["http://localhost:4321/", "https://tu-dominio.com/"];
-
+const allowedOrigins = ['http://localhost:4321'];
 
 const app = express();
 app.use(
   cors({
-    origin: "*",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
 app.use(bodyParser.json());
 app.use(passport.initialize());
-app.use(morgan("dev"));
-
-app.use("/auth", authRouter);
-app.use("/admin", adminRouter);
-app.use("/professors", professorRouter);
-app.use("/courses", courseRouter);
-app.use("/slots", slotRouter);
-app.use("/", helloRouter);
-
-app.get("/public", (req, res) => {
-  res.json({ message: "Public endpoint" });
+// Log body only for POST/PUT/PATCH
+morgan.token('body', (req: any) => {
+  if (!['POST', 'PUT', 'PATCH'].includes(req.method)) return '';
+  return 'body: ' + JSON.stringify(req.body);
 });
 
-app.get("/protected", authenticateJwt, (req, res) => {
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
+
+app.use('/auth', authRouter);
+app.use('/admin', adminRouter);
+app.use('/professors', professorRouter);
+app.use('/courses', courseRouter);
+app.use('/slots', slotRouter);
+app.use('/', helloRouter);
+
+app.get('/public', (req, res) => {
+  res.json({ message: 'Public endpoint' });
+});
+
+app.get('/protected', authenticateJwt, (req, res) => {
   // @ts-ignore
   res.json({ message: `Hello ${req.user.email}` });
 });
